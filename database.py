5395d586a98d5f04e4f1a6c93935c50e22d08598
ian293382 <moshou5571228@gmail.com>
2023-09-11 02:15:50 +0800

    dynamic page for each job

--- database.py
+++ database.py
@@ -1,32 +1,40 @@
 from sqlalchemy import create_engine, text
-
 from dotenv import load_dotenv
 load_dotenv()
 import os
 
-
-
 # "mysql+pymysql://2yze6advjx083prsxef1:pscale_pw_HzDUxzP5anqUVPhPQyhC5bvN0OIGnl16iyEwfRlNTSc@gcp.connect.psdb.cloud/joviancareers?charset=utf8mb4"
-
 
 db_connection_string = os.environ['DB_CONNECTION_STR']
 
 engine = create_engine(
-  db_connection_string,
-  connect_args={
+    db_connection_string,
+    connect_args={
     "ssl": {
-          "ca": "/etc/ssl/cert.pem"
+            "ca": "/etc/ssl/cert.pem"
         }
     })
 
 def load_jobs_from_db():
-  with engine.connect() as connection:
+    with engine.connect() as connection:
       result = connection.execute(text("select * from jobs"))
 
-      jobs = []
-      for row in result.all():
-          jobs.append(dict(row))
-      return jobs
+    jobs = []
+    for row in result.all():
+        jobs.append(dict(row))
+    return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            val=id
+        )
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
 
     # print("type(result):",type(result))
     # result_all = result.all()
