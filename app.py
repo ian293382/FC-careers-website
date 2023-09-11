7d0d11314847aa738918cf0452477781547290ad
ian293382 <moshou5571228@gmail.com>
2023-09-12 03:24:15 +0800

    add api

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-# @app.route("/api/job/<id>")
-# def show_job_json(id):
-#   job = load_job_from_db(id)
-#   return jsonify(job)
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
 
 @app.route("/job/<id>")
 def show_job(id):
