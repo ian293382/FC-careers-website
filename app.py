82fe69f93c13e9b4607469dacd889523380a98da
ian293382 <moshou5571228@gmail.com>
2023-09-12 03:23:53 +0800

    Store submitted application in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+
 
 app = Flask(__name__)
 
@@ -15,6 +16,11 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+# @app.route("/api/job/<id>")
+# def show_job_json(id):
+#   job = load_job_from_db(id)
+#   return jsonify(job)
+
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
@@ -23,6 +29,16 @@ def show_job(id):
 
   return render_template( './jobs/show.html',
                           job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
+
 
 if __name__ == "__main__":
   app.run(port="8081", debug=True)
--- database.py
+++ database.py
@@ -13,6 +13,7 @@ engine = create_engine(
     "ssl": {
             "ca": "/etc/ssl/cert.pem",
         }
+
     })
 
 def load_jobs_from_db():
@@ -52,4 +53,16 @@ def load_job_from_db(id):
 
     # print("type(first_result_dict):", type(first_result_dict))
     # print(first_result_dict) #dict
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
 
+        conn.execute(query,
+                    job_id=job_id,
+                    full_name=data['full_name'],
+                    email=data['email'],
+                    linkedin_url=data['linkedin_url'],
+                    education=data['education'],
+                    work_experience=data['work_experience'],
+                    resume_url=data['resume_url']
+                    )
